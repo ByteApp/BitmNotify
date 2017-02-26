63bb80ea5fbf15ee4affecc834f643a805d31210
MeetByteApp <Meet.ByteApp@gmail.com>
2017-02-26 19:42:15 +0530

    Added button on main activity to test the other activities

--- app/src/main/java/com/example/android/bitmnotify/MainActivity.java
+++ app/src/main/java/com/example/android/bitmnotify/MainActivity.java
@@ -1,13 +1,59 @@
 package com.example.android.bitmnotify;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button b1;
+    Button b2;
+    Button b3;
+    Button b4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b1 = (Button) findViewById(R.id.btn_circulars);
+        b2 = (Button) findViewById(R.id.btn_syllabus);
+        b3 = (Button) findViewById(R.id.btn_resources);
+        b4 = (Button) findViewById(R.id.btn_results);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), CircularActivity.class);
+                startActivity(i);
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), SyllabusActivity.class);
+                startActivity(i);
+            }
+        });
+
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), ResourcesActivity.class);
+                startActivity(i);
+            }
+        });
+
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), ResultActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/android/bitmnotify/SyllabusActivity.java
+++ app/src/main/java/com/example/android/bitmnotify/SyllabusActivity.java
@@ -2,6 +2,7 @@ package com.example.android.bitmnotify;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class SyllabusActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class SyllabusActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_syllabus);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.Toolbar);
+        setSupportActionBar(toolbar);
     }
 }
