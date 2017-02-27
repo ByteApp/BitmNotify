b6f48ffabf017bef28c9e96aaa08c54821f79ec1
MeetByteApp <Meet.ByteApp@gmail.com>
2017-02-27 20:07:02 +0530

    Major Update: Added the Navigation Drawer.

--- app/src/main/java/com/example/android/bitmnotify/CircularActivity.java
+++ app/src/main/java/com/example/android/bitmnotify/CircularActivity.java
@@ -2,6 +2,7 @@ package com.example.android.bitmnotify;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class CircularActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class CircularActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_circular);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
--- app/src/main/java/com/example/android/bitmnotify/ResultActivity.java
+++ app/src/main/java/com/example/android/bitmnotify/ResultActivity.java
@@ -2,6 +2,7 @@ package com.example.android.bitmnotify;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
