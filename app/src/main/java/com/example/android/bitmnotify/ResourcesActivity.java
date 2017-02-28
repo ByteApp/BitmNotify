bc06851ce0203b420fee50baed8fd29aef703ef2
NadeemByteApp <nadeem.byteapp@gmail.com>
2017-02-28 14:39:16 +0530

    added navigation drawer on all activities

--- app/src/main/java/com/example/android/bitmnotify/ResourcesActivity.java
+++ app/src/main/java/com/example/android/bitmnotify/ResourcesActivity.java
@@ -1,10 +1,18 @@
 package com.example.android.bitmnotify;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class ResourcesActivity extends AppCompatActivity {
+public class ResourcesActivity extends AppCompatActivity
+    implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +21,57 @@ public class ResourcesActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            Intent i = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(i);
+        } else if (id == R.id.nav_circulars) {
+            Intent i = new Intent(getApplicationContext(), CircularActivity.class);
+            startActivity(i);
+        } else if (id == R.id.nav_syllabus) {
+            Intent i = new Intent(getApplicationContext(), SyllabusActivity.class);
+            startActivity(i);
+        } else if (id == R.id.nav_resources) {
+            Intent i = new Intent(getApplicationContext(), ResourcesActivity.class);
+            startActivity(i);
+        } else if (id == R.id.nav_results) {
+            Intent i = new Intent(getApplicationContext(), ResultActivity.class);
+            startActivity(i);
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_feedback) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
--- app/src/main/java/com/example/android/bitmnotify/SyllabusActivity.java
+++ app/src/main/java/com/example/android/bitmnotify/SyllabusActivity.java
@@ -1,12 +1,19 @@
 package com.example.android.bitmnotify;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.NavUtils;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
-public class SyllabusActivity extends AppCompatActivity {
+public class SyllabusActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,16 +22,56 @@ public class SyllabusActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            Intent i = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(i);
+        } else if (id == R.id.nav_circulars) {
+            Intent i = new Intent(getApplicationContext(), CircularActivity.class);
+            startActivity(i);
+        } else if (id == R.id.nav_syllabus) {
+            Intent i = new Intent(getApplicationContext(), SyllabusActivity.class);
+            startActivity(i);
+        } else if (id == R.id.nav_resources) {
+            Intent i = new Intent(getApplicationContext(), ResourcesActivity.class);
+            startActivity(i);
+        } else if (id == R.id.nav_results) {
+            Intent i = new Intent(getApplicationContext(), ResultActivity.class);
+            startActivity(i);
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_feedback) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch(item.getItemId()) {
-
-            case android.R.id.home:
-                NavUtils.navigateUpFromSameTask(this);
-                return true;
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
         }
-        return super.onOptionsItemSelected(item);
     }
 }
